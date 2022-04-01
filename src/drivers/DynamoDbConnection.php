c11bf77165e9c678c28c5752aed5e5b42d1453ad
Tim Kelty <tim@pixelandtonic.com>
2022-04-01 00:08:39 -0400

    Connection/session

--- src/drivers/DynamoDbConnection.php
+++ src/drivers/DynamoDbConnection.php
@@ -6,49 +6,36 @@ use Aws\Credentials\CredentialProvider;
 use Aws\Credentials\Credentials;
 use Aws\DynamoDb\DynamoDbClient;
 use Aws\DynamoDb\Exception\DynamoDbException;
+use Aws\DynamoDb\Marshaler;
 use Aws\DynamoDb\WriteRequestBatch;
-use Closure;
-use Exception;
+use JetBrains\PhpStorm\Pure;
 use Yii;
 use yii\base\Component;
+use yii\base\InvalidArgumentException;
 
+/**
+ *
+ * @property callable $credentials
+ * @property null|string $region
+ */
 class DynamoDbConnection extends Component
 {
-    /** @var string Name of table */
     public string $tableName;
-
-    /** @var string Name of hash key in table. Default: "id" */
-    public string $hashKey = 'id';
-
+    public string $partitionKey = 'id';
+    public ?string $partitionKeyPrefix = null;
     public ?string $sortKey = null;
-
-    /** @var string Name of the data attribute in table. Default: "data" */
-    public string $dataAttribute = 'data';
-
-    /** @var string Type of the data attribute in table. Possible values: "S", "B", "N". Default: "S" */
-    public string $dataAttributeType = 'S';
-
-    /** @var bool Whether to use consistent reads */
     public bool $consistentRead = true;
-
-    /** @var array Batch options used for garbage collection */
     public array $batchConfig = [];
-
-    /** @var integer Max time (s) to wait for lock acquisition */
-    public int $maxLockWaitTime = 10;
-
-    /** @var integer Min time (µs) to wait between lock attempts */
-    public int $minLockRetryMicrotime = 10000;
-
-    /** @var integer Max time (µs) to wait between lock attempts */
-    public int $maxLockRetryMicrotime = 50000;
-
-    public ?string $endpoint = null;
     public ?DynamoDbClient $client = null;
     public string $version = 'latest';
     public ?int $ttl = null;
     public string $ttlAttribute = 'ttl';
-    public ?string $keyPrefix = null;
+    public Marshaler $marshaler;
+
+    /**
+     * @var string|null $endpoint Useful for using DAX, local dev
+     */
+    public ?string $endpoint = null;
 
     private ?string $_region = null;
     private $_credentials;
@@ -59,7 +46,125 @@ class DynamoDbConnection extends Component
     public function init(): void
     {
         parent::init();
+        $this->marshaler = $this->getMarshaler();
         $this->client = $this->getClient();
+    }
+
+    public function getItem($key): ?array
+    {
+        try {
+            $result = $this->client->getItem([
+                'TableName'      => $this->tableName,
+                'Key'            => $this->formatKey($key),
+                'ConsistentRead' => $this->consistentRead,
+            ]);
+        } catch (DynamoDbException $e) {
+            Yii::error("Unable to get item: {$e->getMessage()}");
+            return null;
+        }
+
+        $item = $result['Item'] ?? null;
+
+        return $item ? $this->marshaler->unmarshalItem($item) : null;
+    }
+
+    public function updateItem($key, $data = null): bool
+    {
+        $attributes = [];
+
+        if ($data) {
+            $attributes += $data;
+        }
+
+        if ($this->ttl) {
+            $attributes += [
+                $this->ttlAttribute => time() + $this->ttl,
+            ];
+        }
+
+        try {
+            return (bool) $this->client->updateItem([
+                'TableName'        => $this->tableName,
+                'Key'              => $this->formatKey($key),
+                'AttributeUpdates' => $this->_marshalAttributeValues($attributes),
+            ]);
+        } catch (DynamoDbException $e) {
+            Yii::error("Unable to update item: {$e->getMessage()}");
+            return false;
+        }
+    }
+
+    public function deleteItem($key): bool
+    {
+        try {
+            return (bool) $this->client->deleteItem([
+                'TableName' => $this->tableName,
+                'Key'       => $this->formatKey($key),
+            ]);
+        } catch (DynamoDbException $e) {
+            Yii::error("Unable to delete item: {$e->getMessage()}");
+            return false;
+        }
+    }
+
+    public function deleteExpired(): void
+    {
+        $scan = $this->client->getPaginator('Scan', [
+            'TableName' => $this->tableName,
+            'AttributesToGet' => [$this->partitionKey],
+            'ScanFilter' => [
+                $this->ttlAttribute => [
+                    'ComparisonOperator' => 'LT',
+                    'AttributeValueList' => [
+                        $this->marshaler->marshalValue(time()),
+                    ],
+                ],
+            ],
+        ]);
+
+        // Create a WriteRequestBatch for deleting the expired items
+        $batch = new WriteRequestBatch($this->client, $this->batchConfig);
+
+        // Perform Scan and BatchWriteItem (delete) operations as needed
+        foreach ($scan->search('Items') as $item) {
+            $batch->delete(
+                [$this->partitionKey => $item[$this->partitionKey]],
+                $this->tableName,
+            );
+        }
+
+        // Delete any remaining items that were not auto-flushed
+        $batch->flush();
+    }
+
+    protected function formatKey($key): array
+    {
+        $sortKey = null;
+
+        if (is_array($key)) {
+            if (!$this->sortKey) {
+                throw new InvalidArgumentException('A sort key must be provided to use compound keys.');
+            }
+
+            if (count($key) !== 2) {
+                throw new InvalidArgumentException('Compound keys must be an array with exactly 2 elements.');
+            }
+
+            [$partitionKey, $sortKey] = $key;
+        } else {
+            $partitionKey = $key;
+        }
+
+        $partitionKey = "{$this->partitionKeyPrefix}$partitionKey";
+        $keyValue = [
+            $this->partitionKey => $partitionKey,
+        ];
+
+        if ($sortKey && $this->sortKey) {
+            $keyValue[$this->sortKey] = $sortKey;
+        }
+
+        return $this->marshaler->marshalItem($keyValue);
     }
 
     protected function getClient(): DynamoDbClient
@@ -72,171 +177,12 @@ class DynamoDbConnection extends Component
             'credentials' => $this->getCredentials(),
             'region' => $this->getRegion(),
             'version' => $this->version,
-
-            // useful for using DAX, local dev
             'endpoint' => $this->endpoint,
         ]);
 
         $this->client = new DynamoDbClient($config);
 
         return $this->client;
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function read($key, $locking = false): ?array
-    {
-        if ($locking) {
-            return $this->_readWithPessimisticLock($key);
-        }
-
-        try {
-            // Execute a GetItem command to retrieve the item.
-            $result = $this->client->getItem([
-                'TableName'      => $this->tableName,
-                'Key'            => $this->formatKey($key),
-                'ConsistentRead' => $this->consistentRead,
-            ]);
-
-            // Get the item values
-            return  $result['Item'] ?? [];
-        } catch (DynamoDbException $e) {
-
-            // TODO: why?
-            return null;
-        }
-    }
-
-    /**
-     * @param string $key
-     * @param null|mixed $data The data to write to the data attribute. If `null`, no data will be written,
-     *                         but ttl will still be updated.
-     * @return bool Whether write was successful
-     */
-    public function write(string $key, mixed $data): bool
-    {
-        $attributes = [
-            'lock' => ['Action' => 'DELETE'],
-        ];
-
-        if ($this->ttl) {
-            $attributes[$this->ttlAttribute] = time() + $this->ttl;
-        }
-
-        if ($data !== null) {
-            if ($data) {
-                $attributes[$this->dataAttribute] = [
-                    'Value' => [
-                        $this->dataAttributeType => $data
-                    ]
-                ];
-            } else {
-                $attributes[$this->dataAttribute] = ['Action' => 'DELETE'];
-            }
-        }
-
-        try {
-            return (bool) $this->client->updateItem([
-                'TableName'        => $this->tableName,
-                'Key'              => $this->formatKey($key),
-                'AttributeUpdates' => $attributes,
-            ]);
-        } catch (DynamoDbException $e) {
-            Yii::error("Error writing “{$key}”: {$e->getMessage()}");
-            return false;
-        }
-    }
-
-    public function delete($key): bool
-    {
-        try {
-            return (bool) $this->client->deleteItem([
-                'TableName' => $this->tableName,
-                'Key'       => $this->formatKey($key),
-            ]);
-        } catch (DynamoDbException $e) {
-            Yii::error("Error deleting “{$key}”: {$e->getMessage()}");
-            return false;
-        }
-    }
-
-    public function deleteExpired(): void
-    {
-        // Create a Scan iterator for finding expired items
-        $scan = $this->client->getPaginator('Scan', [
-            'TableName' => $this->tableName,
-            'AttributesToGet' => [$this->hashKey],
-            'ScanFilter' => [
-                $this->ttlAttribute => [
-                    'ComparisonOperator' => 'LT',
-                    'AttributeValueList' => [['N' => (string) time()]],
-                ],
-                'lock' => [
-                    'ComparisonOperator' => 'NULL',
-                ]
-            ],
-        ]);
-
-        // Create a WriteRequestBatch for deleting the expired items
-        $batch = new WriteRequestBatch($this->client, $this->batchConfig);
-
-        // Perform Scan and BatchWriteItem (delete) operations as needed
-        foreach ($scan->search('Items') as $item) {
-            $batch->delete(
-                [$this->hashKey => $item[$this->hashKey]],
-                $this->tableName
-            );
-        }
-
-        // Delete any remaining items that were not auto-flushed
-        $batch->flush();
-    }
-
-    protected function formatKey(string $key): array
-    {
-        return [
-            $this->hashKey => ['S' => "{$this->keyPrefix}$key"]
-        ];
-    }
-
-    /**
-     * @throws Exception
-     */
-    private function _readWithPessimisticLock(string $key): ?array
-    {
-        // Create the params for the UpdateItem operation so that a lock can be
-        // set and item returned (via ReturnValues) in a one, atomic operation.
-        $params = [
-            'TableName'        => $this->tableName,
-            'Key'              => $this->formatKey($key),
-            'Expected'         => ['lock' => ['Exists' => false]],
-            'AttributeUpdates' => ['lock' => ['Value' => ['N' => '1']]],
-            'ReturnValues'     => 'ALL_NEW',
-        ];
-
-        // Acquire the lock and fetch the item data.
-        $timeout  = time() + $this->maxLockWaitTime;
-        while (true) {
-            try {
-                $result = $this->client->updateItem($params);
-                return $result['Attributes'] ?? null;
-            } catch (DynamoDbException $e) {
-                if (
-                    $e->getAwsErrorCode() === 'ConditionalCheckFailedException' &&
-                    time() < $timeout
-                ) {
-                    usleep(random_int(
-                        $this->minLockRetryMicrotime,
-                        $this->maxLockRetryMicrotime
-                    ));
-                } else {
-                    break;
-                }
-            }
-        }
-
-        return null;
     }
 
     protected function getCredentials(): callable
@@ -266,5 +212,17 @@ class DynamoDbConnection extends Component
     protected function getRegion(): ?string
     {
         return $this->_region ?? (getenv('AWS_REGION') ?: null) ?? (getenv('AWS_DEFAULT_REGION') ?: null);
+    }
+
+    #[Pure] protected function getMarshaler(): Marshaler
+    {
+        return $this->marshaler ?? new Marshaler();
+    }
+
+    private function _marshalAttributeValues(array $values): array
+    {
+        return array_map(fn($value) => [
+            'Value' => $this->marshaler->marshalValue($value),
+        ], $values);
     }
 }
--- src/drivers/DynamoDbSession.php
+++ src/drivers/DynamoDbSession.php
@@ -2,27 +2,38 @@
 
 namespace pixelandtonic\dynamodb\drivers;
 
-use pixelandtonic\dynamodb\WithDynamoDbClient;
-use Yii;
+use Aws\DynamoDb\SessionConnectionConfigTrait;
+use Aws\DynamoDb\SessionHandler;
+use yii\di\Instance;
 use yii\web\Session;
 
 class DynamoDbSession extends Session
 {
-    use WithDynamoDbClient;
+    use SessionConnectionConfigTrait;
+
+    /**
+     * @var DynamoDBConnection|string|array the DynamoDB [[Connection]] object or the application component ID of the DynamoDB [[Connection]].
+     * This can also be an array that is used to create a DynamoDB [[Connection]] instance in case you do not want do configure
+     * a DynamoDB connection as an application component.
+     * After the Cache object is created, if you want to change this property, you should only assign it
+     * with a DynamoDB [[Connection]] object.
+     */
+    public $dynamoDb = 'dynamoDb';
 
     /**
      * @inheritDoc
      */
     public function init(): void
     {
-        if ($this->keyPrefix === null) {
-            $this->keyPrefix = substr(md5(Yii::$app->id), 0, 5);
-        }
-
-        $this->client = $this->getClient();
-        $this->handler = DynamoDbSessionHandler::fromClient($this->client, [
-            'keyPrefix' => $this->keyPrefix
+        $this->dynamoDb = Instance::ensure($this->dynamoDb, DynamoDbConnection::class);
+        $this->handler = SessionHandler::fromClient($this->dynamoDb->client, [
+            // 'session_lifetime' => 30,
         ]);
         parent::init();
+    }
+
+    public function getUseCustomStorage(): bool
+    {
+        return true;
     }
 }
