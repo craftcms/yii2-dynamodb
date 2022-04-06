605d6b462e96db5080128915a05bd952a933f03a
Tim Kelty <tim@pixelandtonic.com>
2022-04-06 12:43:24 -0400

    Add types

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,22 +2,28 @@
 
 namespace tests;
 
-use yii\caching\CacheInterface;
-use yii\web\Session;
+use pixelandtonic\dynamodb\drivers\DynamoDbCache;
+use pixelandtonic\dynamodb\drivers\DynamoDbQueue;
+use pixelandtonic\dynamodb\drivers\DynamoDbSession;
 
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
-    protected function getCache(): CacheInterface
+    protected function getDynamoDb(): DynamoDbConnection
+    {
+        return \Yii::$app->getDynamoDb();
+    }
+
+    protected function getCache(): DynamoDbCache
     {
         return \Yii::$app->getCache();
     }
 
-    protected function getSession(): Session
+    protected function getSession(): DynamoDbSession
     {
         return \Yii::$app->getSession();
     }
 
-    protected function getQueue(string $id = 'queue')
+    protected function getQueue(string $id = 'queue'): DynamoDbQueue
     {
         return \Yii::$app->get($id);
     }
